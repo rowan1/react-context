3ad47e1ef4f16470297cfd240751796ceb610c79
Rowan Ibrahim <rowan.ibrahim@yomicepa.com>
2020-04-17 00:05:59 +0200

    creating language selector via context

---
+++ translate/src/components/LanguageSelector.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+
+class LanguageSelector extends React.Component{
+    static contextType = LanguageContext;
+    render(){
+        return(
+            <div>
+                Select a language:
+                <i 
+                    className="flag us" 
+                    onClick={()=>{this.context.onLanguageChange('english')}}
+                />
+                <i 
+                    className="flag nl" 
+                    onClick={()=>{this.context.onLanguageChange('dutsh')}}
+                />
+            </div>
+        )
+    }
+}
+export default LanguageSelector;
